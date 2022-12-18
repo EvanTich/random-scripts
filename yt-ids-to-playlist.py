0bd4dbd10df2dea8e3c7e00f402e636338566b30
Evan Tichenor <evan.tichenor@gmail.com>
2022-12-18 12:11:25 -0800

    fixed the yt-ids-to-playlist.py script

--- yt-ids-to-playlist.py
+++ yt-ids-to-playlist.py
@@ -5,7 +5,7 @@ from sys import argv
 def main(filename):
 
     # ask user for personal youtube headers
-    YTMusic.setup()
+    headers = YTMusic.setup()
 
     # get all the video ids in the file
     with open(filename) as f:
@@ -17,17 +17,18 @@ def main(filename):
                 print('"(?)" contained in music file. Please use video ids you know are correct.')
                 print('Stopping execution.')
                 return
-            video_id = line[-1]
+            video_id = line.split()[-1]
             video_ids.append(video_id)
 
     # create the playlist
-    ytm = YTMusic()
+    ytm = YTMusic(headers)
     title = input('Playlist title: ')
     desc = input('Playlist Description: ')
     yt_id = ytm.create_playlist(title, desc, video_ids=video_ids)
 
     # return the playlist id if it was created successfully
     if isinstance(yt_id, str):
+        print('Playlist created:', yt_id)
         return yt_id
     print('Error occurred:', yt_id)
     return None
